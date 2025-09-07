0bc9438fdab57ed4300c49b751651d29e1035a61
Exorcist11 <dungnguyenx2077@gmail.com>
2025-09-07 22:41:58 +0700

    set prefix api

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,14 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
   // Exception filter global
   app.useGlobalFilters(new AllExceptionsFilter());
 
   // Response interceptor global
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
